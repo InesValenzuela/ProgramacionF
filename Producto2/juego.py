856dcff6594a8f0e71e225f48868f472c738bf24
InesValenzuela <InesValenzuelaC10@gmail.com>
2015-02-18 16:19:47 -0700

    Agragando producto2

---
+++ Producto2/juego.py
@@ -0,0 +1,15 @@
+# Hello World in Python
+import time
+print "Hola! Trataré de adivinar un número. Piensa un número entre 1 y 10."
+import time
+time.sleep(5)
+print "Ahora multiplicalo por 9."
+import time
+time.sleep(5)
+print "Si el número tiene 2 dígitos, súmalos entre si: Ej. 36 -> 3+6=9. Si tu número tiene un solo dígito, súmale 0."
+import time
+time.sleep(5)
+print "Al número resultante súmale 4."
+import time
+time.sleep(10)
+print "Muy bien. El resultado es 13 :3 "
